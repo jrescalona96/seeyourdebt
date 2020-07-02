b06bba315cd499b06d24dcbbe009035ea900f077
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-02 00:59:55 -0700

    display total paid amount

--- src/components/totalDebt.jsx
+++ src/components/totalDebt.jsx
@@ -3,18 +3,18 @@ import { Spring } from "react-spring/renderprops";
 
 function TotalDebt({ total, previousBalance, balance, currencyFormatter }) {
   const percentage = (balance / total) * 100;
-  const textHeight = "12rem";
+  const textHeight = "10rem";
 
   function getColor() {
-    if (percentage <= 10) return "#0390fc";
-    else if (percentage <= 20) return "#03d7fc";
-    else if (percentage <= 30) return "#0331fc";
-    else if (percentage <= 40) return "#03d7fc";
-    else if (percentage <= 50) return "#73fc03";
-    else if (percentage <= 60) return "#fcf403";
-    else if (percentage <= 70) return "#fcbe03";
-    else if (percentage <= 80) return "#fc6b03";
-    else if (percentage > 80) return "#fc0303";
+    if (percentage <= 10) return "#0390fcaf";
+    else if (percentage <= 20) return "#03d7fcaf";
+    else if (percentage <= 30) return "#0331fcaf";
+    else if (percentage <= 40) return "#03d7fcaf";
+    else if (percentage <= 50) return "#73fc03af";
+    else if (percentage <= 60) return "#fcf403af";
+    else if (percentage <= 70) return "#fcbe03af";
+    else if (percentage <= 80) return "#fc6b03af";
+    else if (percentage > 80) return "#fc0303af";
     else return "gray";
   }
 
@@ -28,12 +28,13 @@ function TotalDebt({ total, previousBalance, balance, currencyFormatter }) {
     <React.Fragment>
       <Spring
         config={{ tension: 25, friction: 10 }}
-        from={{ number: previousBalance }}
-        to={{ number: balance }}
+        from={{ number: previousBalance, number2: balance }}
+        to={{ number: balance, number2: total - balance }}
       >
         {(props) => (
-          <div id="totalDebt" className="text-center" style={style}>
-            {currencyFormatter.format(props.number.toFixed())}
+          <div id="totalDebt" style={style}>
+            <div>Paid:{currencyFormatter.format(props.number2.toFixed())}</div>
+            <div>Bal:{currencyFormatter.format(props.number.toFixed())}</div>
           </div>
         )}
       </Spring>
