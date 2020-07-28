44348b22404f22ef6c2472637ceb4f8734d8d417
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-28 11:59:04 -0700

    Updates

---
+++ precache-manifest.7f77844ce575ea3ab4ab304fe872a0a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0e6033b5bcd868b473cdfabe230970",
+    "url": "/debtcounter/index.html"
+  },
+  {
+    "revision": "91305fd222960aa28f0c",
+    "url": "/debtcounter/static/css/2.419068d4.chunk.css"
+  },
+  {
+    "revision": "8b3365acb402302d88ee",
+    "url": "/debtcounter/static/css/main.c39ab624.chunk.css"
+  },
+  {
+    "revision": "91305fd222960aa28f0c",
+    "url": "/debtcounter/static/js/2.5ccb5683.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/debtcounter/static/js/2.5ccb5683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3365acb402302d88ee",
+    "url": "/debtcounter/static/js/main.ae4313bf.chunk.js"
+  },
+  {
+    "revision": "de072e119b1467981a1f",
+    "url": "/debtcounter/static/js/runtime-main.c001aee9.js"
+  },
+  {
+    "revision": "0277c785a3840d5552a683eb740b46cb",
+    "url": "/debtcounter/static/media/concrete-wall.0277c785.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/debtcounter/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/debtcounter/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/debtcounter/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/debtcounter/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/debtcounter/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "23583502493b44bd428ad321901a1eba",
+    "url": "/debtcounter/static/media/logo.23583502.png"
+  }
+]);
