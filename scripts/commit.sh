6f625c639428004b9bfe465a994159e6a3a4df85
John Richmond Escalona <mond@Johns-MacBook-Pro.local>
2020-07-28 12:05:15 -0700

    Deploy gh-pages site, edit gitignore

--- scripts/commit.sh
+++
@@ -1,8 +0,0 @@
-#commit, merge, push changes from dev
-git checkout master && git merge dev
-git add . && git commit -m $1 && echo "commited with ==> " $1
-echo "merged changes"
-git push
-echo "pushed changes"
-git checkout dev
-cd scripts
